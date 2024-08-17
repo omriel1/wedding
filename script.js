d69190ec038b9b22f2fa60d991f8a7758b03b851
OmriLevyTau <omrilevy@mail.tau.ac.il>
2024-08-17 20:58:52 +0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.querySelectorAll('.map-point').forEach(point => {
     point.addEventListener('click', function() {
         const descriptionBox = document.getElementById('description');
         descriptionBox.textContent = this.getAttribute('data-description');
-        descriptionBox.style.display = 'block';
+        descriptionBox.classList.add('active');
+
+        setTimeout(() => {
+            descriptionBox.classList.remove('active');
+        }, 5000); // Hide the description after 5 seconds
     });
 });
