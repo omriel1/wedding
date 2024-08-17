6e6f2d5aea96e3fcaa24508b7de0bd07000e6dbf
OmriLevyTau <omrilevy@mail.tau.ac.il>
2024-08-17 21:05:37 +0300

    update css

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
+let isDragging = false;
+let startX, startY, initialScrollLeft, initialScrollTop;
+
+const mapContainer = document.querySelector('.map-container');
+
+mapContainer.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.pageX - mapContainer.offsetLeft;
+    startY = e.pageY - mapContainer.offsetTop;
+    initialScrollLeft = mapContainer.scrollLeft;
+    initialScrollTop = mapContainer.scrollTop;
+});
+
+mapContainer.addEventListener('mouseleave', () => {
+    isDragging = false;
+});
+
+mapContainer.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+mapContainer.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - mapContainer.offsetLeft;
+    const y = e.pageY - mapContainer.offsetTop;
+    const walkX = x - startX;
+    const walkY = y - startY;
+    mapContainer.scrollLeft = initialScrollLeft - walkX;
+    mapContainer.scrollTop = initialScrollTop - walkY;
+});
+
+mapContainer.addEventListener('touchstart', (e) => {
+    isDragging = true;
+    const touch = e.touches[0];
+    startX = touch.pageX - mapContainer.offsetLeft;
+    startY = touch.pageY - mapContainer.offsetTop;
+    initialScrollLeft = mapContainer.scrollLeft;
+    initialScrollTop = mapContainer.scrollTop;
+});
+
+mapContainer.addEventListener('touchend', () => {
+    isDragging = false;
+});
+
+mapContainer.addEventListener('touchmove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const touch = e.touches[0];
+    const x = touch.pageX - mapContainer.offsetLeft;
+    const y = touch.pageY - mapContainer.offsetTop;
+    const walkX = x - startX;
+    const walkY = y - startY;
+    mapContainer.scrollLeft = initialScrollLeft - walkX;
+    mapContainer.scrollTop = initialScrollTop - walkY;
+});
+
 document.querySelectorAll('.map-point').forEach(point => {
-    point.addEventListener('click', function() {
+    point.addEventListener('click', function () {
         const descriptionBox = document.getElementById('description');
         descriptionBox.textContent = this.getAttribute('data-description');
         descriptionBox.classList.add('active');
